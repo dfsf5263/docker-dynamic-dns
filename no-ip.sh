8a2a71179b5ffc5f9918bb156de24892fce299b2
dfsf5263 <dfsf5263@gmail.com>
2019-09-26 20:15:08 -0400

    Formatting. Validations.

--- no-ip.sh
+++ no-ip.sh
@@ -5,7 +5,7 @@ then
 	echo "No user was set. Use -u=username"
 	exit 10
 else
-    USER=${USER}
+  USER=${USER}
 fi
 
 if [[ -z "${PASSWORD}" ]]
@@ -13,7 +13,7 @@ then
 	echo "No password was set. Use -p=password"
 	exit 20
 else
-    PASSWORD=${PASSWORD}
+  PASSWORD=${PASSWORD}
 fi
 
 if [[ -z "${HOSTNAME}" ]]
@@ -21,21 +21,21 @@ then
 	echo "No host name. Use -h=host.example.com"
 	exit 30
 else
-    HOSTNAME=${HOSTNAME}
+  HOSTNAME=${HOSTNAME}
 fi
 
 if [[ -z "${SERVICE}" ]]
 then
 	SERVICE="noip"
 else
-    SERVICE=${SERVICE}
+  SERVICE=${SERVICE}
 fi
 
 if [[ -n "${DETECTIP}" ]]
 then
 	IP=$(wget -qO- "http://myexternalip.com/raw")
 else
-    DETECTIP=${DETECTIP}
+  DETECTIP=${DETECTIP}
 fi
 
 if [[ -n "${DETECTIP}" ]] && [[ -z ${IP} ]]
@@ -43,44 +43,41 @@ then
 	RESULT="Could not detect external IP."
 fi
 
-
 if [[ ${INTERVAL} != [0-9]* ]]
 then
 	echo "Interval is not an integer."
 	exit 35
 else
-    INTERVAL=${INTERVAL}
+  INTERVAL=${INTERVAL}
 fi
-
 
 SERVICEURL="dynupdate.no-ip.com/nic/update"
 NOIPURL="https://${USER}:${PASSWORD}@"
 
 case "${SERVICE}" in
-        noip)
-            SERVICEURL="dynupdate.no-ip.com/nic/update"
-            ;;
+  noip)
+    SERVICEURL="dynupdate.no-ip.com/nic/update"
+    ;;
 
-        dyndns)
-            SERVICEURL="members.dyndns.org/v3/update"
-            ;;
+  dyndns)
+    SERVICEURL="members.dyndns.org/v3/update"
+    ;;
 
-        duckdns)
-            SERVICEURL="www.duckdns.org/v3/update"
-            ;;
+  duckdns)
+    SERVICEURL="www.duckdns.org/v3/update"
+    ;;
 
-		google)
-            SERVICEURL="domains.google.com/nic/update"
-            ;;
+	google)
+    SERVICEURL="domains.google.com/nic/update"
+    ;;
 
-		dynudns)
-            SERVICEURL="api.dynu.com/nic/update"
-            NOIPURL="https://"
-            ;;
+	dynudns)
+    SERVICEURL="api.dynu.com/nic/update"
+    NOIPURL="https://"
+    ;;
 
-        *)
-			SERVICEURL="dynupdate.no-ip.com/nic/update"
-
+  *)
+		SERVICEURL="dynupdate.no-ip.com/nic/update"
 esac
 
 USERAGENT="--user-agent=\"no-ip shell script/1.0 mail@mail.com\""
@@ -105,30 +102,30 @@ then
 	then
 		NOIPURL="${NOIPURL}&"
 	fi
-	SHA256PASSWORD=$(printf ${PASSWORD} | sha256sum | cut -d " " -f 1)
+	SHA256PASSWORD=$(printf "%s" "${PASSWORD}" | sha256sum | cut -d " " -f 1)
 	NOIPURL="${NOIPURL}username=${USER}&password=${SHA256PASSWORD}"
 fi
 
 while :
 do
 
-    if [[ -n "${HOSTNAME}" ]] || [[ ${SERVICE} == "dynudns" ]]
-    then
-        TEMPURL="${NOIPURL}&"
-    fi
-    IP=$(wget -qO- "http://myexternalip.com/raw")
-    TEMPURL="${NOIPURL}myip=$IP"
+  if [[ -n "${HOSTNAME}" ]] || [[ ${SERVICE} == "dynudns" ]]
+  then
+    TEMPURL="${NOIPURL}&"
+  fi
+  IP=$(wget -qO- "http://myexternalip.com/raw")
+  TEMPURL="${NOIPURL}myip=$IP"
 
-    if [[ ${SERVICE} == "dynudns" ]]
-    then
-        echo "${TEMPURL}"
-	    RESULT=$(wget --no-check-certificate -qO- ${TEMPURL})
-	else
-	    echo "${AUTHHEADER} ${USERAGENT} ${TEMPURL}"
-	    RESULT=$(wget --no-check-certificate -qO- ${AUTHHEADER} ${USERAGENT} ${TEMPURL})
-    fi
+  if [[ ${SERVICE} == "dynudns" ]]
+  then
+    echo "${TEMPURL}"
+    RESULT=$(wget --no-check-certificate -qO- ${TEMPURL})
+  else
+    echo "${AUTHHEADER} ${USERAGENT} ${TEMPURL}"
+    RESULT=$(wget --no-check-certificate -qO- "${AUTHHEADER} ${USERAGENT} ${TEMPURL}")
+  fi
 
-	echo ${RESULT}
+	echo "${RESULT}"
 
 	if [[ ${INTERVAL} -eq 0 ]]
 	then
